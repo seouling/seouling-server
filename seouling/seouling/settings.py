0b30a9e095f91c33ffabfe62a0f6e20d017fed8d
eunseo Lee <eunseo9808@naver.com>
2019-09-28 01:13:19 +0900

    deploy

--- seouling/seouling/settings.py
+++ seouling/seouling/settings.py
@@ -80,19 +80,19 @@ WSGI_APPLICATION = 'seouling.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': os.getenv('DATABASE_URL'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.getenv('DATABASE_URL'),
+    }
+}
 
 
 # Password validation
