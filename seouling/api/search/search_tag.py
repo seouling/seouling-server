de6c4f9572f8e72c247d115267d3f65bc3c78ea0
eunseo Lee <eunseo9808@naver.com>
2019-09-28 14:51:20 +0900

    gu / tag 수정

--- seouling/api/search/search_tag.py
+++ seouling/api/search/search_tag.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.core.paginator import Paginator
 from utils.page_serializer import PageSerializer
 from django.db.models import Subquery, OuterRef
+from utils.gu import kr_gu
 
 
 class SQCount(Subquery):
@@ -20,6 +21,11 @@ class SearchTag(APIView):
         tags = request.data.get('tags', [])
         page = request.query_params.get('page', 1)
 
+        if len(gu) == 0:
+            gu = [i for i in range(len(kr_gu))]
+
+        if len(tags) == 0:
+            tags = [-1]
         spot_query = Spot.objects.filter(gu__in=gu)\
             .annotate(
                 rank=SQCount(SpotTag.objects.filter(spot_id=OuterRef("pk"), tag_id__in=tags).values('pk'))
